f497df8125892752c65e429f8e701a521d3d771a
Владимир Пилипенко <vovapilip46@gmail.com>
2023-10-11 01:07:29 +0500

    Добавьте файлы проекта.

---
+++ Mini_array.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	const int COOL = 10;
+	const int ROOL = 10;
+
+	int arr[COOL][ROOL];
+
+	for (int i = 0; i < COOL; i++)
+	{
+		for (int j = 0; j < ROOL; j++)
+		{
+			arr[i][j] = rand() % 10;
+		}
+	}
+
+	for (int i = 0; i < COOL; i++)
+	{
+		for (int j = 0; j < ROOL; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
